266fc06c35ec9e5d7697487728717c26e2e3c4b8
Grzegorz Grabowski <grzegorz.grabowski@intel.com>
2016-10-18 19:34:08 +0200

    Added selection of output folder\nCode reformatted

--- rename.sh
+++ rename.sh
@@ -31,12 +31,14 @@ LOG_FILE=""
 CP=1 
 ROTATE=0
 COMPRESS=0
-ROOT=""
+SORT=0
+
+BASE_DIR=`pwd`
+DIR_OUT=`pwd`
 
 # ============================= 
 function show_help
 {
- # FIX IT description missing
     echo "Usage: rename.sh [-o target_directory] [-d] [-m] [-l log_file] [-r] [-c compression_level]"
     echo "   -o   copy/move renamed files to target_directory and create directory structure"
     echo "   -d   display debug messages "
@@ -44,13 +46,15 @@ function show_help
     echo "   -l   log messages into log_file"
     echo "   -r   automatically rotate files"
     echo "   -c   compress files with compression_level"
+    echo "   -s   sort files intro folders (by month)"
+# TO DO -i option (input folder)
     exit 1
 }
 
 # A POSIX variable
 OPTIND=1 # Reset in case getopts has been used previously in the shell.
 
-while getopts "h?c:rmvl:do:" opt; do
+while getopts "h?c:rmvl:do:s" opt; do
     case "$opt" in
     h|\?)
         show_help
@@ -68,7 +72,9 @@ while getopts "h?c:rmvl:do:" opt; do
         ;;
     l)  LOG_FILE=$OPTARG
         ;;
-    o)  ROOT=$OPTARG
+    s)  SORT=1
+        ;;
+    o)  DIR_OUT=$OPTARG
         ;;
     esac
 done
@@ -82,106 +88,122 @@ echo "ROTATE=$ROTATE"
 echo "COPY=$CP"
 echo "DEBUG=$DEBUG"
 echo "LOG_FILE='$LOG_FILE'"
-echo "ROOT='$ROOT'"
+echo "DIR_OUT='$DIR_OUT'"
 echo "VERBOSE=$VERBOSE"
+echo "SORT=$SORT"
 echo "Leftovers: $@"
+
+# FIX IT option error check
 
 # ============================= 
 
-      
+function exists {
+  command -v "$1" >/dev/null 2>&1
+}
+    
 function count {
 c=0
 extensions="jpg jpeg png"
 for ext in $extensions; do
-  c_=$(find $1 -maxdepth 10 -iname "*.$ext" -print0 | tr -d -c "\000" | wc -c)
+  c_=$(find "$1" -maxdepth 10 -iname "*.$ext" -print0 | tr -d -c "\000" | wc -c)
   c=$(($c+$c_))
 done
-echo $c
+#echo $c
 exit $c
 }
 
 function log {
-  if [ "$LOG_FILE" !=  "" ] ; then
-    echo -e "$@"
-      if [ "$LOG_FILE" != "" ] ; then
-        echo -e "$@" >> $LOG_FILE
-      fi
-  fi
+	if [ "$VERBOSE" ==  "1" ] ; then
+		echo -e "$@"
+	fi
+	
+	if [ "$LOG_FILE" != "" ] ; then
+		echo -e "$@" >> $LOG_FILE
+	fi
 }  
 
 function debug {
-  if [ "$DEBUG" = 1 ] ; then
-    echo -e "DEBUG : $@"
-    if [ "$LOG_FILE" != "" ] ; then
-      echo -e "DEBUG : $@" >> $LOG_FILE
-    fi
-  fi
+	if [ "$DEBUG" = 1 ] ; then
+    	echo -e "DEBUG : $@"
+	fi
+	
+	if [ "$LOG_FILE" != "" ] ; then
+		echo -e "DEBUG : $@" >> $LOG_FILE
+	fi
 }  
 
 # count files before
-FILES_IN=`count .` 
+FILES_IN=`count "$BASE_DIR"` 
+
+
+log "BASE_DIR=$BASE_DIR"
+
 
 function rename {
-  file=$1
-  log "++++++++++++++++++++++++++"
-
-  # take action on each file. $f store current file name  
-  COUNTER=$(($COUNTER + 1))
-  log "Processing file $file ($COUNTER/$FILES_IN)"
-
-  # get directory
-  DIR_IN=`echo "$file" | grep -Eo '.*[/]'`
   
-   # get file extension
-  EXT=${file##*.}
+file=$1
+log "++++++++++++++++++++++++++"
+
+# take action on each file. $f store current file name  
+COUNTER=$(($COUNTER + 1))
+log "Processing file $file ($COUNTER/$FILES_IN)"
+
+# get directory
+DIR_IN=`echo "$file" | grep -Eo '.*[/]'`
   
-  #get short file name (no extension) 
-  FILE_NAME=`echo "$file" | rev | cut -d / -f 1 | sed 's/^[^.]*.//g' | rev`
+# get file extension
+EXT=${file##*.}
+  
+# get short file name (no extension) 
+FILE_NAME_IN=`echo "$file" | rev | cut -d / -f 1 | sed 's/^[^.]*.//g' | rev`
    
-  # read creation time from metadata
-  CREATED=`exiftool "$file" | grep -m 1 "Create"`
+# read creation time from metadata
+CREATED=`exiftool "$file" | grep -m 1 "Create"`
 
-  #extract date and time and reformat
-  DATE=`echo $CREATED | grep -Eo '[0-9]{4}:[0-9]{2}:[0-9]{2}' | awk -F '[:]' '{print $1"-"$2"-"$3}'`
-  TIME=`echo $CREATED | grep -Eo '[0-9]{2}:[0-9]{2}:[0-9]{2}$' | awk -F '[:]' '{print $1"_"$2"_"$3}'`
+#extract date and time and reformat
+DATE=`echo $CREATED | grep -Eo '[0-9]{4}:[0-9]{2}:[0-9]{2}' | awk -F '[:]' '{print $1"-"$2"-"$3}'`
+TIME=`echo $CREATED | grep -Eo '[0-9]{2}:[0-9]{2}:[0-9]{2}$' | awk -F '[:]' '{print $1"_"$2"_"$3}'`
   
-  # read model name and replace spaces with underscore
-  MODEL=`exiftool "$file" | grep "Camera Model Name" | cut -d : -f 2 | cut -c 2-30 | tr ' ' '_'`
+# read model name and replace spaces with underscore
+MODEL=`exiftool "$file" | grep "Camera Model Name" | cut -d : -f 2 | cut -c 2-30 | tr ' ' '_'`
 
-  # build output file name
-  BASE="$DATE $TIME $MODEL"
+# build output file name
+if [ "$TIME" == "" ] ; then
+	debug " NO METADATA in file $file !! "
+	FILE_NAME_OUT=`echo "$FILE_NAME_IN" | sed 's/_NO_METADATA//'`
+else
+	FILE_NAME_OUT="$DATE $TIME $MODEL"
+fi
   
-  DIR="$DIR_IN"
+debug "FILE_NAME_IN  : $FILE_NAME_IN"
+debug "FILE_NAME_OUT : $FILE_NAME_OUT"
+      
+DIR="$DIR_IN"
   
-  debug "DIR_IN $DIR_IN"
-  debug "DIR $DIR"
+debug "DIR_IN $DIR_IN"
+debug "DIR $DIR"
   
-  if [ "$ROOT" != "" ] ; then
-    debug "Changing root to $ROOT"
-    DIR="$ROOT/"
+if [ "$SORT" == "1" ] ; then
+	debug "Sorting photos into folders"
 
-  	FOLDER=`echo $CREATED | grep -Eo '[0-9]{4}:[0-9]{2}:[0-9]{2}' | awk -F '[:]' '{print $1"-"$2}'`
-	DIR="$DIR$FOLDER/"
-    if [ "$TIME" == "" ] ; then
-	  DIR="$DIR/NO_METADATA/"	
+	if [ "$TIME" == "" ] ; then
+		FOLDER="NO_METADATA"	
+	else
+		FOLDER=`echo $CREATED | grep -Eo '[0-9]{4}:[0-9]{2}:[0-9]{2}' | awk -F '[:]' '{print $1"-"$2}'`
 	fi
-  fi
 
- if [ ! -d "$DIR" ] ; then
-      debug "Creating directory $DIR"
-      mkdir -p "$DIR"
-   fi
-   
-  if [ "$TIME" == "" ] ; then
-    debug " NO METADATA !! "
+	DIR="$DIR_OUT/SORTED/$FOLDER/"
+else
+	DIR="$DIR_OUT/"
+fi
+ 	
+if [ ! -d "$DIR" ] ; then
+	debug "Creating directory $DIR"
+	mkdir -p "$DIR"
+fi
 
-    FILE_IN=`echo $FILE_NAME | sed 's/_NO_METADATA//'`
-    debug "OUTPUT $OUTPUT"
-  	BASE="$FILE_IN""_NO_METADATA"
-  	debug "$OUTPUT"
-  fi
-
-   OUTPUT="$DIR$BASE.$EXT"  
+OUTPUT="$DIR$FILE_NAME_OUT.$EXT"
+debug "OUTPUT <- $OUTPUT"  
 
 POSTFIX=""
 MD_IN=`md5 "$file" | awk -F '[=]' '{print $2}'` 
@@ -192,6 +214,10 @@ while [ "$PROCESSED" = 0 ] ; do
 
 MD_OUT=`md5 "$OUTPUT" | awk -F '[=]' '{print $2}'`
 
+
+debug "IN  : $MD_IN :$file"
+debug "OUT : $MD_OUT : $OUTPUT"
+	
 # if same file (same md5) exists do nothing
 if [ -f "$OUTPUT" ] && [ "$MD_IN" == "$MD_OUT" ] ; then
 	log "PICTURE : File $file exists"
@@ -201,7 +227,7 @@ elif [ -f "$OUTPUT" ] && [ "$MD_IN" != "$MD_OUT" ] ; then
 	debug "$OUTPUT"
     POSTFIX=$POSTFIX"_"
     # FIX IT postfix if no metadata
-    OUTPUT="$DIR$BASE$POSTFIX.$EXT"
+    OUTPUT="$DIR$FILE_NAME_OUT$POSTFIX.$EXT"
 else
 	debug "$OUTPUT"
   	debug "$MD_IN"
@@ -210,17 +236,17 @@ else
 	MODIFIED=$(($MODIFIED+1))
 	
 	if [ "$CP" = 1 ] ; then
-	  log "PICTURE : Copying file ($MODIFIED) $file to $OUTPUT"
-	  cp "$file" "$OUTPUT"	
+		log "PICTURE : Copying file ($MODIFIED) $file to $OUTPUT"
+		cp "$file" "$OUTPUT"	
 	else
-	  log "PICTURE : Moving file ($MODIFIED) $file to $OUTPUT"
-	  mv "$file" "$OUTPUT"	
+		log "PICTURE : Moving file ($MODIFIED) $file to $OUTPUT"
+	  	mv "$file" "$OUTPUT"	
 	fi
-	
+	# FIX IT
 	if [ "$ROTATE" = "1" ] ; then
 		jhead $OUTPUT
 	fi
-	
+	# FIX IT
 	if [ "$COMPRESS" = "1" ] ; then
 		log "PICTURE : Compressing file $OUTPUT"
 		convert -compress jpeg -quality 40 $OUTPUT $OUTPUT
@@ -230,28 +256,17 @@ else
 fi
 done
 
-log " ======================== "
+log " ================================= "
 }
 
 while read -d '' -r file; do
-    rename "$file"
-done < <(find . -type f \( -name "*.jpg" -or -name "*.jpeg" -or -name "*.JPG" \) -print0)
+	rename "$file"
+done < <(find "$BASE_DIR" -type f \( -name "*.jpg" -or -name "*.jpeg" -or -name "*.JPG" \) -print0)
 
 log "MODIF=$MODIFIED"
 # count files after
-if [ "$ROOT" != "" ] ; then
-  FILES_OUT=`count $ROOT`
-else
-  FILES_OUT=`count .`
-fi
+FILES_OUT=`count "$DIR_OUT"`
 
-# TO DO expand path
-# FIX count files in output folder
-# FIX IT diplay full folder name
-log "Files before processing in $DIR_IN : $FILES_IN"
-if [ "$ROOT" != "" ] ; then
-  log "Files after processing in $ROOT : $FILES_OUT"
-else
-  log "Files after processing in ./ : $FILES_OUT"
-fi
+log "Files before processing in $BASE_DIR : $FILES_IN"
+log "Files after processing in $DIR_OUT : $FILES_OUT"
 log "Files moved/copied      : $MODIFIED"
